f4ceeaa465ef53085863fe9af6425615147b8595
Raimundas Radavicius <raimundas_radavicius@yahoo.com>
2023-06-07 11:33:52 +0100

    components for budget/ramaining/spent

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Budget from "./components/Budget";
+import Remaining from "./components/Ramaining";
+import Spent from "./components/Spent";
 
 const App = () => {
-  return <p>Hello World!</p>;
+  return (
+    <div className="container">
+      <h1 className="mt-3">MyBudget</h1>
+      <div className="row mt-3">
+        <div className="col-sm">
+          <Budget />
+          <Remaining />
+          <Spent />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
