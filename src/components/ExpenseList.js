47522197e12c548002d366f3302b847e5c51af59
Raimundas Radavicius <raimundas_radavicius@yahoo.com>
2023-06-09 12:10:11 +0100

    expenses list uses app context

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,14 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
 import ExpenseItem from "./ExpenseItem";
+import { AppContext } from "../context/AppContext";
 
 const ExpenseList = () => {
-  const expenses = [
-    { id: 1, name: "Shopping", cost: 50 },
-    { id: 2, name: "Transport", cost: 80 },
-    { id: 3, name: "Groceries", cost: 100 },
-    { id: 4, name: "Utility", cost: 200 },
-    { id: 5, name: "Rent", cost: 1200 },
-  ];
+  const { expenses } = useContext(AppContext);
 
   return (
     <ul className="list-group">
